4f1d233f30d46856e9193a67cbe1229a38670c96
Kryss118 <christian.berti@gmail.com>
2022-10-31 01:40:11 +0100

    aggiornato

--- src/components/ElencoComponent.js
+++ src/components/ElencoComponent.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+
 
 const ElencoComponent = () => {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    const [banda, setBanda] = useState('Scegli Banda')
+
     const elencoBande = [
         {name: "Mercenari di Middenheim", id: 0},
         {name: "Mercenari di Reikland", id: 1},
@@ -13,16 +20,45 @@ const ElencoComponent = () => {
         {name: "Skaven", id: 7},
     ];
     
+    const renderLabel = () => {
+        if (value || isFocus) {
+          return (
+            <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+              Bande:
+            </Text>
+          );
+        }
+        return null;
+      };
+      console.log(banda)
     return (
         <View>
             <Text style={styles.stileTesto}>Bande</Text>
-            <FlatList
-                data={elencoBande}
-                showsVerticalScrollIndicator={false}
-                renderItem={({item}) => <Text>{item.name}</Text>}
-                keyExtractor={(item, key) => item.id}
-            />
-            <Text>qui sopra dovrebbe esserci la lista...</Text>
+            <View style={styles.container}>
+                {renderLabel()}
+                <Dropdown
+                    style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    ///inputSearchStyle={styles.inputSearchStyle}
+                    ///iconStyle={styles.iconStyle}
+                    data={elencoBande}
+                    ///search
+                    maxHeight={300}
+                    labelField="name"
+                    valueField="name"
+                    placeholder={!isFocus ? banda : ''}
+                    ///searchPlaceholder="Search..."
+                    value={value}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                    onChange={item => {
+                        setValue(item.value);
+                        setIsFocus(false);
+                        setBanda(item.name)
+                    }}
+                />
+            </View>
         </View>
     )
 };
@@ -32,7 +68,39 @@ const styles = StyleSheet.create({
             fontSize: 20,
             textAlign: 'center',
             fontWeight: 'bold'
-        }
+        },
+        container: {
+            backgroundColor: 'white',
+            padding: 16,
+          },
+          dropdown: {
+            height: 50,
+            borderColor: 'gray',
+            borderWidth: 0.5,
+            borderRadius: 8,
+            paddingHorizontal: 8,
+          },
+          label: {
+            position: 'absolute',
+            backgroundColor: 'white',
+            left: 22,
+            top: 8,
+            zIndex: 999,
+            paddingHorizontal: 8,
+            fontSize: 14,
+          },
+          placeholderStyle: {
+            textAlign: 'center',
+            fontSize: 16,
+            fontWeight: 'bold'
+          },
+          selectedTextStyle: {
+            fontSize: 16,
+          },
+          inputSearchStyle: {
+            height: 40,
+            fontSize: 16,
+          },
 });
 
 export default ElencoComponent;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchInput from '../components/SearchInput';
 import ElencoComponent from '../components/ElencoComponent';
